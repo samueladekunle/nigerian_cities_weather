a77e1b6f1d793210662e880fb4d2b226461f994b
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2022-10-24 00:37:00 +0100

    Add app colors class

---
+++ lib/src/themes/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  const AppColors._();
+
+  static const accentColor = Color(0xFF7aaff9);
+  static const scaffoldBlueColor = Color(0xFF005495);
+  static const rainBlueLight = Color(0xFF4480C6);
+}
