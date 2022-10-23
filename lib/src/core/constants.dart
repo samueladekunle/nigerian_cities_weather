0381cce5a45df1fa76aa053b02b7dde02bf63196
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2022-10-24 00:19:58 +0100

    Add constants

---
+++ lib/src/core/constants.dart
@@ -0,0 +1,12 @@
+/* ----------- Error messages ----------- */
+const kDefaultErrorMessage = 'An error occurred, please try again later';
+
+const kNoInternetMessage = 'No Internet connection';
+
+/* ------- API status codes -------- */
+const kUnauthorisedStatusCode = 401;
+
+const kForbiddenStatusCode = 403;
+
+/* ---------- Type definitions */
+typedef JsonResponse = Map<String, dynamic>;
