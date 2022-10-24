5c4ac5626a2ca1de3cd918c7ffe4992d9a44640f
Samuel Adekunle <oluwatomisinadekunle@gmail.com>
2022-10-24 01:38:29 +0100

    Add weather icon image widget

---
+++ lib/src/features/weather/presentation/widgets/weather_icon_image.dart
@@ -0,0 +1,29 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class WeatherIconImage extends StatelessWidget {
+  const WeatherIconImage({
+    super.key,
+    required this.iconUrl,
+    required this.size,
+  });
+
+  final String iconUrl;
+  final double size;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: iconUrl,
+      width: size,
+      height: size,
+      errorWidget: (context, url, error) {
+        return const Icon(
+          Icons.cloud,
+          color: Colors.white,
+          size: 60,
+        );
+      },
+    );
+  }
+}
